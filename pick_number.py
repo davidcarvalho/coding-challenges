9dfb067add5a88e4199f19e8741defc84e733e4d
david carvalho <davidcarvalho@users.noreply.github.com>
2020-07-17 16:43:26 +0530

    Add first challenge statement

---
+++ pick_number.py
@@ -0,0 +1,5 @@
+# Pick a number that the user will try to stop at. You can either pick this number for them or you can ask the user to input a target time.
+
+# Once they press Enter to begin, the timer will start and the user will have to "guess" when that amount of time has passed and press Enter again.
+
+# Print out the elapsed time and tell the user if they won or by how much they were over or under
